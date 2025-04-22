3e67ffbed89d9d642ac0a09e6c621f5d579eb741
Bill Cauchois <wcauchois@gmail.com>
2025-04-21 20:15:12 -0400

    compression

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 mod utils;
 
+use std::io::{BufWriter, Cursor};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, lzma-rs!");
+pub fn compress(input: &[u8]) -> Vec<u8> {
+    let mut input_buf = Cursor::new(input);
+    let mut output_buf_writer = BufWriter::new(Vec::new());
+    lzma_rs_inner::lzma_compress(&mut input_buf, &mut output_buf_writer).unwrap();
+    output_buf_writer.into_inner().unwrap()
 }
