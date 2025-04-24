d9d13a98abed401d4f30a8b5edaacd8efd3216f5
Bill Cauchois <wcauchois@gmail.com>
2025-04-23 21:06:05 -0400

    add decompress

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,11 @@ pub fn compress(input: &[u8]) -> Vec<u8> {
     lzma_rs_inner::lzma_compress(&mut input_buf, &mut output_buf_writer).unwrap();
     output_buf_writer.into_inner().unwrap()
 }
+
+#[wasm_bindgen]
+pub fn decompress(input: &[u8]) -> Vec<u8> {
+    let mut input_buf = Cursor::new(input);
+    let mut output_buf_writer = BufWriter::new(Vec::new());
+    lzma_rs_inner::lzma_decompress(&mut input_buf, &mut output_buf_writer).unwrap();
+    output_buf_writer.into_inner().unwrap()
+}
